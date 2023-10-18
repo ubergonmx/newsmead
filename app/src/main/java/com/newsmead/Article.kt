c7593b90b6dcabbccda27234c4b02d0569d4acd5
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-10-19 07:10:04 +0800

    Add sample imgs & `DataHelper` & `Article` classes

---
+++ app/src/main/java/com/newsmead/Article.kt
@@ -0,0 +1,41 @@
+package com.newsmead
+
+class Article {
+    var source: String
+        private set
+    var sourceImage : Int
+        private set
+    var title: String
+        private set
+    var imageId: Int
+        private set
+    var date: String
+        private set
+    var readTime: String
+        private set
+    var url: String
+        private set
+
+    constructor(source: String, sourceImage: Int, title: String, imageId: Int, date: String, readTime: String, url: String) {
+        this.source = source
+        this.sourceImage = sourceImage
+        this.title = title
+        this.imageId = imageId
+        this.date = date
+        this.readTime = readTime
+        this.url = url
+    }
+
+    // Creates an article with limited information. Only source, title, date, and readTime are
+    // initialized. This is meant for testing purposes.
+    constructor(source: String, title: String, date: String, readTime: String, url: String) {
+        this.source = source
+        this.sourceImage = R.drawable.sample_source_image
+        this.title = title
+        this.imageId = R.drawable.sample_article_image
+        this.date = date
+        this.readTime = readTime
+        this.url = url
+    }
+
+}
---
+++ app/src/main/java/com/newsmead/DataHelper.kt
@@ -0,0 +1,65 @@
+package com.newsmead
+
+import java.util.ArrayList
+
+// This is for testing purposes only. You can delete this class.
+class DataHelper {
+    fun loadArticleData(): ArrayList<Article> {
+        val data = ArrayList<Article>()
+        data.add(
+            Article(
+                "CNN",
+                "Biden's first 100 days: What he's gotten done",
+                "April 29, 2021",
+                "5 min read",
+                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
+            )
+        )
+        data.add(
+            Article(
+                "CNN",
+                "Biden's first 100 days: What he's gotten done",
+                "April 29, 2021",
+                "5 min read",
+                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
+            )
+        )
+        data.add(
+            Article(
+                "CNN",
+                "Biden's first 100 days: What he's gotten done",
+                "April 29, 2021",
+                "5 min read",
+                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
+            )
+        )
+        data.add(
+            Article(
+                "CNN",
+                "Biden's first 100 days: What he's gotten done",
+                "April 29, 2021",
+                "5 min read",
+                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
+            )
+        )
+        data.add(
+            Article(
+                "CNN",
+                "Biden's first 100 days: What he's gotten done",
+                "April 29, 2021",
+                "5 min read",
+                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
+            )
+        )
+        data.add(
+            Article(
+                "CNN",
+                "Biden's first 100 days: What he's gotten done",
+                "April 29, 2021",
+                "5 min read",
+                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
+            )
+        )
+        return data
+    }
+}
