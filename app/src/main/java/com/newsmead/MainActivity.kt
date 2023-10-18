9a0b6887b482de0c198003d669a190965760134c
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-10-19 05:56:58 +0800

    Implement navbar function & add Home design

--- app/src/main/java/com/newsmead/MainActivity.kt
+++ app/src/main/java/com/newsmead/MainActivity.kt
@@ -2,6 +2,9 @@ package com.newsmead
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.crashlytics.ktx.setCustomKeys
@@ -11,11 +14,19 @@ import com.newsmead.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Initialize viewBinding for MainActivity
         this.viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(this.viewBinding.root)
+
+        // Initialize navController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        this.navController = navHostFragment.navController
+        val bottomNavigationView = this.viewBinding.bottomNavigationView
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
