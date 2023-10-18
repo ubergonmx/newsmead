eed677d289833b2d0ee2806b7e26ea5827a52104
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-10-19 03:33:12 +0800

    Update `setContentView` with `viewBinding`

--- app/src/main/java/com/newsmead/MainActivity.kt
+++ app/src/main/java/com/newsmead/MainActivity.kt
@@ -6,18 +6,16 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.crashlytics.ktx.setCustomKeys
 import com.google.firebase.ktx.Firebase
+import com.newsmead.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val crashlytics = Firebase.crashlytics
-        crashlytics.setCustomKeys {
-            key("current_level", 1)
-            key("last_UI_action", "empty_state")
-        }
-        Firebase.crashlytics.log("Test log")
-        throw RuntimeException("Test Crash 3")
+        // Initialize viewBinding for MainActivity
+        this.viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.viewBinding.root)
     }
 }
