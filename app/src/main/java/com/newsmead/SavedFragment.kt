539c58f4a539f6c71e7b4618a76654180f21c0ca
erik-lance <eriklance@gmail.com>
2023-10-20 05:16:41 +0800

    Added viewbinding for fragment

--- app/src/main/java/com/newsmead/SavedFragment.kt
+++ app/src/main/java/com/newsmead/SavedFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.newsmead.databinding.FragmentSavedBinding
+import com.newsmead.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class SavedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved, container, false)
+        val binding = FragmentSavedBinding.inflate(
+            inflater, container, false
+        )
+
+
+        return binding.root
     }
 
     companion object {
