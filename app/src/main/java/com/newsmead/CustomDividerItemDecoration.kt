ad466e90d1b0a775c1b0c26a7489cf3e40c4c961
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-10-20 03:56:28 +0800

    Fix issue #11 and refactor divider item class

--- app/src/main/java/com/newsmead/CustomDividerItemDecoration.kt
+++ app/src/main/java/com/newsmead/CustomDividerItemDecoration.kt
@@ -7,21 +7,24 @@ import android.view.View
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-class CustomDividerItemDecoration(context: Context?, @DrawableRes dividerRes: Int) : RecyclerView.ItemDecoration() {
+class CustomDividerItemDecoration(context: Context?, @DrawableRes dividerRes: Int, private val excludedViewHolderClass: Class<out RecyclerView.ViewHolder>) : RecyclerView.ItemDecoration() {
 
     private val mDivider: Drawable? = context?.let { ContextCompat.getDrawable(it, dividerRes) }
-
     override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         val left = parent.paddingLeft
         val right = parent.width - parent.paddingRight
         val childCount = parent.childCount
-        for (i in 1 until childCount) {
+        for (i in 0 until childCount) {
             val child: View = parent.getChildAt(i)
             val params = child.layoutParams as RecyclerView.LayoutParams
             val top: Int = child.bottom + params.bottomMargin
             val bottom = top + mDivider?.intrinsicHeight!! ?: 0
-            mDivider?.setBounds(left, top, right, bottom)
-            mDivider?.draw(c)
+            val viewHolder = parent.getChildViewHolder(child)
+            if (!excludedViewHolderClass.isInstance(viewHolder)) {
+                mDivider?.setBounds(left, top, right, bottom)
+                mDivider?.draw(c)
+            }
+
         }
     }
 }
