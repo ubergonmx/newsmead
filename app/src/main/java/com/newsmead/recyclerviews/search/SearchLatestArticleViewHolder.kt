92ed06bb784e18bd98057c1b77cbea245a69f612
erik-lance <eriklance@gmail.com>
2023-10-20 04:00:51 +0800

    Fixed `SearchArticleViewHolder` extension

--- app/src/main/java/com/newsmead/recyclerviews/search/SearchLatestArticleViewHolder.kt
+++ app/src/main/java/com/newsmead/recyclerviews/search/SearchLatestArticleViewHolder.kt
@@ -1,11 +1,16 @@
 package com.newsmead.recyclerviews.search
 
 import androidx.recyclerview.widget.RecyclerView
+import com.newsmead.Article
 import com.newsmead.databinding.ItemFeedArticleSimplifiedBinding
 class SearchLatestArticleViewHolder(private val viewBinding: ItemFeedArticleSimplifiedBinding): RecyclerView.ViewHolder(viewBinding.root) {
 
         // Performs the binding of the article data to the views in the ViewHolder
-        fun bindData() {
-
+        fun bindData(article: Article) {
+            this.viewBinding.tvSource.text = article.source
+            this.viewBinding.ivSourceImage.setImageResource(article.sourceImage)
+            this.viewBinding.tvArticleTitle.text = article.title
+            this.viewBinding.tvArticleDate.text = article.date
+            this.viewBinding.tvReadTime.text = article.readTime
         }
 }
