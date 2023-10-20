ff891e7cb412deb7be6dfdfe9218dcb3900fb27b
erik-lance <eriklance@gmail.com>
2023-10-20 17:56:14 +0800

    Loads article feeds under list

--- app/src/main/java/com/newsmead/fragments/saved/SavedListArticlesFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedListArticlesFragment.kt
@@ -5,13 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.newsmead.DataHelper.loadArticleData
+import com.newsmead.FeedArticleAdapter
 import com.newsmead.R
-
+import com.newsmead.databinding.FragmentSavedListArticlesBinding
 class SavedListArticlesFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_saved_list_articles, container, false)
+        val binding = FragmentSavedListArticlesBinding.inflate(inflater, container, false)
+
+        val data = loadArticleData()
+        val adapter = FeedArticleAdapter(data)
+        binding.rvSavedArticles.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.rvSavedArticles.layoutManager = layoutManager
+
+        return binding.root
     }
 }
