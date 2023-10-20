e1b4e773f8f9589ed091fa83b13c68f61b81662e
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-10-20 13:54:54 +0800

    Added tab layout mediator for saved

--- app/src/main/java/com/newsmead/SavedFragment.kt
+++ app/src/main/java/com/newsmead/SavedFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.newsmead.databinding.FragmentSavedBinding
 import com.newsmead.databinding.FragmentSearchBinding
 
@@ -24,6 +25,8 @@ class SavedFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentSavedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -37,13 +40,23 @@ class SavedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentSavedBinding.inflate(
-            inflater, container, false
-        )
+        binding = FragmentSavedBinding.inflate(inflater, container, false)
 
-        binding.vpViewPager.adapter = TabFragmentAdapter(this)
+
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.vpViewPager.adapter = TabFragmentAdapter(this)
+
+        val tabLayout = binding.tlListTabs
+        TabLayoutMediator(tabLayout, binding.vpViewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = "Your Lists"
+                1 -> tab.text = "All"
+            }
+        }.attach()
     }
 
     companion object {
