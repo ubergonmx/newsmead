df223c0443cdd1a27c07bbb21e65713e25babd60
erik-lance <eriklance@gmail.com>
2023-10-20 16:10:40 +0800

    Added recyclerview functionality to SavedAll

--- app/src/main/java/com/newsmead/fragments/saved/SavedAllFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedAllFragment.kt
@@ -5,13 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.newsmead.R
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.newsmead.DataHelper.loadArticleData
+import com.newsmead.FeedArticleAdapter
+import com.newsmead.databinding.FragmentSavedAllBinding
 class SavedAllFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_saved_all, container, false)
+        val binding = FragmentSavedAllBinding.inflate(inflater, container, false)
+
+        // Data generation
+        val data = loadArticleData()
+
+        // use feed item recyclerview adapter
+        binding.rvSavedAll.adapter = FeedArticleAdapter(data)
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.rvSavedAll.layoutManager = layoutManager
+
+
+        return binding.root
     }
 }
