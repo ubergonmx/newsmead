17d059510595c5781d201b2ff698d2ee9fe82752
erik-lance <eriklance@gmail.com>
2023-10-20 22:05:01 +0800

    Added fragment logic to prepare articlefragment

--- app/src/main/java/com/newsmead/ArticleActivity.kt
+++ app/src/main/java/com/newsmead/ArticleActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import com.newsmead.databinding.ActivityArticleBinding
+import com.newsmead.fragments.article.ArticleFragment
 
 class ArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,19 @@ class ArticleActivity : AppCompatActivity() {
         // Initialize viewBinding for ArticleActivity
         val binding = ActivityArticleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val articleId = intent.getStringExtra("articleId")
+
+        if (articleId != null) {
+            val articleFragment = ArticleFragment()
+            val args = Bundle()
+            args.putString("articleId", articleId)
+            articleFragment.arguments = args
+
+            // Replace the fragment_container view with the ArticleFragment
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.flMainArticleContainer, articleFragment)
+                .commit()
+        }
     }
 }
