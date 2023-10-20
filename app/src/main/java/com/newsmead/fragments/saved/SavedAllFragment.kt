793b965c565cb9db2f45119b67650cf0172231ed
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-10-20 13:40:37 +0800

    Added initial saved fragments

--- app/src/main/java/com/newsmead/fragments/saved/SavedAllFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedAllFragment.kt
@@ -1,4 +1,17 @@
 package com.newsmead.fragments.saved
 
-class SavedAllFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.newsmead.R
+
+class SavedAllFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_saved_all, container, false)
+    }
 }
