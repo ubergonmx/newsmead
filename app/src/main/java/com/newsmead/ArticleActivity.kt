aeec8c7c94948af5741359bef198d5eae85ba5ec
erik-lance <eriklance@gmail.com>
2023-10-20 21:29:01 +0800

    Added databinding to ArticleActivity

--- app/src/main/java/com/newsmead/ArticleActivity.kt
+++ app/src/main/java/com/newsmead/ArticleActivity.kt
@@ -3,9 +3,14 @@ package com.newsmead
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.newsmead.databinding.ActivityArticleBinding
+
 class ArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article)
+
+        // Initialize viewBinding for ArticleActivity
+        val binding = ActivityArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
