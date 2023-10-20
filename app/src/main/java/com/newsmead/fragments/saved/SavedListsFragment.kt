8472191719470ad731e16428201510a0501bf238
erik-lance <eriklance@gmail.com>
2023-10-20 16:26:46 +0800

    Added recyclerview functionality for saved lists

--- app/src/main/java/com/newsmead/fragments/saved/SavedListsFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedListsFragment.kt
@@ -4,13 +4,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.newsmead.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.newsmead.DataHelper.loadListData
+import com.newsmead.databinding.FragmentSavedYourListsBinding
+import com.newsmead.recyclerviews.saved.SavedListsAdapter
 
 class SavedListsFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_saved_your_lists, container, false)
+        val binding = FragmentSavedYourListsBinding.inflate(inflater, container, false)
+
+        val data = loadListData()
+
+        binding.rvSavedLists.adapter = SavedListsAdapter(data)
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        binding.rvSavedLists.layoutManager = layoutManager
+
+        return binding.root
     }
 }
