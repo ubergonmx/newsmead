bb73a5b39ad623090880c37edcfcdd4778a918a0
erik-lance <eriklance@gmail.com>
2023-10-21 14:25:14 +0800

    Added divider decoration for saved lists all articles

--- app/src/main/java/com/newsmead/fragments/saved/SavedAllFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedAllFragment.kt
@@ -7,8 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.newsmead.DataHelper.loadArticleData
+import com.newsmead.R
+import com.newsmead.custom.CustomDividerItemDecoration
 import com.newsmead.recyclerviews.feed.FeedArticleAdapter
 import com.newsmead.databinding.FragmentSavedAllBinding
+import com.newsmead.recyclerviews.feed.FeedHeaderViewHolder
+
 class SavedAllFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,10 @@ class SavedAllFragment: Fragment() {
         val layoutManager = LinearLayoutManager(context)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         binding.rvSavedAll.layoutManager = layoutManager
+
+        // Add divider
+        val customDivider = CustomDividerItemDecoration(context, R.drawable.line_divider, FeedHeaderViewHolder::class.java)
+        binding.rvSavedAll.addItemDecoration(customDivider)
 
 
         return binding.root
