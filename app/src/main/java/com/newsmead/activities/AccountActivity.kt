9f719cbd516db22da9aa6da0975c85af612d5a86
erik-lance <eriklance@gmail.com>
2023-10-21 16:30:39 +0800

    Added starting view to account activity

--- app/src/main/java/com/newsmead/activities/AccountActivity.kt
+++ app/src/main/java/com/newsmead/activities/AccountActivity.kt
@@ -2,11 +2,24 @@ package com.newsmead.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.newsmead.R
+
+import com.newsmead.databinding.AccountActivityBinding
+import com.newsmead.fragments.account.SignUpFragment
 
 class AccountActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: AccountActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.account_activity)
+
+        this.viewBinding = AccountActivityBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // Display Starting Fragment (Sign Up)
+        val signUpFragment = SignUpFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(this.viewBinding.flAccountContainer.id, signUpFragment)
+            .commit()
+
     }
 }
