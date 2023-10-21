3ee567ffbae9e1ee518c496834ba2efb5a41633e
erik-lance <eriklance@gmail.com>
2023-10-21 14:47:53 +0800

    Added initial history fragment

---
+++ app/src/main/java/com/newsmead/fragments/pages/HistoryFragment.kt
@@ -0,0 +1,38 @@
+package com.newsmead.fragments.pages
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.newsmead.DataHelper
+import com.newsmead.databinding.FragmentHistoryBinding
+import com.newsmead.recyclerviews.feed.FeedArticleSimplifiedAdapter
+
+class HistoryFragment: Fragment() {
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+        // Back Button
+        binding.btnHistoryBack.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+        // RecyclerView
+        val data = DataHelper.loadArticleData()
+        binding.rvHistory.adapter = FeedArticleSimplifiedAdapter(data)
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.rvHistory.layoutManager = layoutManager
+
+
+
+        return binding.root
+    }
+}
