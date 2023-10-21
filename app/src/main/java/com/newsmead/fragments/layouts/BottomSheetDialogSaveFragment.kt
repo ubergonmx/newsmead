7b98c76e9a26736b6b06c1b31cd61bd0a7688eee
erik-lance <eriklance@gmail.com>
2023-10-21 12:01:28 +0800

    Added sheet fragment

---
+++ app/src/main/java/com/newsmead/fragments/layouts/BottomSheetDialogSaveFragment.kt
@@ -0,0 +1,36 @@
+package com.newsmead.fragments.layouts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.newsmead.DataHelper
+import com.newsmead.databinding.BotttomSheetDialogSaveListBinding
+import com.newsmead.recyclerviews.dialog.SheetDialogAdapter
+
+class BottomSheetDialogSaveFragment: BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = BotttomSheetDialogSaveListBinding.inflate(inflater, container, false)
+
+        binding.btnListDone.setOnClickListener {
+            // Save details here
+
+            dismiss()
+        }
+
+        // Mount recyclerView here
+        val data = DataHelper.loadListNamesData()
+        binding.rvDialogList.adapter = SheetDialogAdapter(data)
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.rvDialogList.layoutManager = layoutManager
+
+        return binding.root
+    }
+}
