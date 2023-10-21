b941fbd632e002dc0581e42039c684b0f62e3fa7
erik-lance <eriklance@gmail.com>
2023-10-21 15:27:05 +0800

    Added new account activity

---
+++ app/src/main/java/com/newsmead/activities/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.newsmead.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.newsmead.R
+
+class AccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.account_activity)
+    }
+}
