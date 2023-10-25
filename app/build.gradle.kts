9df51e01efcf3afb8b9bc4302d312a1f22cb0eb8
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-10-26 06:16:51 +0800

    Add sample NewsAPI code (RetroFit and Gson)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,10 @@ dependencies {
     // Import Splash Screen dependencies
     implementation("androidx.core:core-splashscreen:1.0.0")
 
+    // Import Retrofit and Gson dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
