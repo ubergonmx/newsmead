f9f05255e87b0741d5912812f67c8d1bd6d28977
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-11-01 03:20:40 +0800

    Add ArticleObject (temporary class for NewsAPI)

--- app/src/main/java/com/newsmead/models/NewsResponse.kt
+++ app/src/main/java/com/newsmead/models/NewsResponse.kt
@@ -3,5 +3,21 @@ package com.newsmead.models
 data class NewsResponse (
     val status: String,
     val totalResults: Int,
-    val articles: List<Article> = emptyList()
+    val articles: List<ArticleObject> = emptyList()
 )
+
+data class ArticleObject (
+    val source: SourceObject,
+    val author: String?,
+    val title: String,
+    val description: String?,
+    val url: String,
+    val urlToImage: String?,
+    val publishedAt: String,
+    val content: String?
+) {
+    class SourceObject {
+        val id: String? = null
+        val name: String? = null
+    }
+}
