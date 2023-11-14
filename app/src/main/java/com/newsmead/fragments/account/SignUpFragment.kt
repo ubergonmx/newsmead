0ea9eff3410b892af3d9202d9cd1da4f4af44dc1
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-11-15 03:31:15 +0800

    Added initial firebase logic for register

--- app/src/main/java/com/newsmead/fragments/account/SignUpFragment.kt
+++ app/src/main/java/com/newsmead/fragments/account/SignUpFragment.kt
@@ -1,20 +1,26 @@
 package com.newsmead.fragments.account
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.newsmead.R
 
 import com.newsmead.databinding.FragmentSignUpBinding
 
 class SignUpFragment: Fragment() {
     private lateinit var viewBinding: FragmentSignUpBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         this.viewBinding = FragmentSignUpBinding.inflate(inflater, container, false)
+        this.auth = FirebaseAuth.getInstance()
 
         // Buttons
         this.viewBinding.btnAccLog.setOnClickListener {
@@ -26,12 +32,6 @@ class SignUpFragment: Fragment() {
         }
 
         this.viewBinding.btnAccStart.setOnClickListener {
-            // Goes to Onboarding Fragment
-//            val onboardingFragment = OnboardingFragment()
-//            requireActivity().supportFragmentManager.beginTransaction()
-//                .replace(R.id.flAccountContainer, onboardingFragment)
-//                .commit()
-
             val email = this.viewBinding.etAccEmail.text.toString()
             val password = this.viewBinding.etAccPassword.text.toString()
             val confirmPassword = this.viewBinding.etAccConfirmPassword.text.toString()
@@ -73,6 +73,32 @@ class SignUpFragment: Fragment() {
             }
 
             // Create user with email and password
+            this.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = this.auth.currentUser
+                        user!!.sendEmailVerification()
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    // Email sent.
+                                    this.viewBinding.etAccEmail.setText("")
+                                    this.viewBinding.etAccPassword.setText("")
+                                    this.viewBinding.etAccConfirmPassword.setText("")
+                                    this.viewBinding.etAccEmail.requestFocus()
+
+                                    // Goes to Onboarding Fragment
+                                    finishSignUp()
+                                }
+                            }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                         Toast.makeText(requireActivity(), "Authentication failed.",
+                             Toast.LENGTH_SHORT).show()
+                        // updateUI(null)
+                    }
+                }
         }
 
         this.viewBinding.cbViewPassword.setOnClickListener {
@@ -88,5 +114,13 @@ class SignUpFragment: Fragment() {
         }
 
         return this.viewBinding.root
+    }
+
+    fun finishSignUp() {
+        // Goes to Onboarding Fragment
+        val onboardingFragment = OnboardingFragment()
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.flAccountContainer, onboardingFragment)
+            .commit()
     }
 }
