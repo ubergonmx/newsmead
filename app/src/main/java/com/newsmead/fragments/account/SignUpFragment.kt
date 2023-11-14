bb2382ad4875bc11066b04a21a083e585b92368c
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-11-15 02:46:12 +0800

    Added initial sign up

--- app/src/main/java/com/newsmead/fragments/account/SignUpFragment.kt
+++ app/src/main/java/com/newsmead/fragments/account/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.newsmead.fragments.account
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,10 +27,52 @@ class SignUpFragment: Fragment() {
 
         this.viewBinding.btnAccStart.setOnClickListener {
             // Goes to Onboarding Fragment
-            val onboardingFragment = OnboardingFragment()
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.flAccountContainer, onboardingFragment)
-                .commit()
+//            val onboardingFragment = OnboardingFragment()
+//            requireActivity().supportFragmentManager.beginTransaction()
+//                .replace(R.id.flAccountContainer, onboardingFragment)
+//                .commit()
+
+            val email = this.viewBinding.etAccEmail.text.toString()
+            val password = this.viewBinding.etAccPassword.text.toString()
+            val confirmPassword = this.viewBinding.etAccConfirmPassword.text.toString()
+
+            if (email.isEmpty()) {
+                this.viewBinding.etAccEmail.error = "Email is required"
+                this.viewBinding.etAccEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                this.viewBinding.etAccEmail.error = "Please provide valid email"
+                this.viewBinding.etAccEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                this.viewBinding.etAccPassword.error = "Password is required"
+                this.viewBinding.etAccPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 4) {
+                this.viewBinding.etAccPassword.error = "Min password length should be 4 characters"
+                this.viewBinding.etAccPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (confirmPassword.isEmpty()) {
+                this.viewBinding.etAccConfirmPassword.setError("Confirm Password is required")
+                this.viewBinding.etAccConfirmPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!password.equals(confirmPassword)) {
+                this.viewBinding.etAccConfirmPassword.setError("Password and Confirm Password does not match")
+                this.viewBinding.etAccConfirmPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            // Create user with email and password
         }
 
         this.viewBinding.cbViewPassword.setOnClickListener {
