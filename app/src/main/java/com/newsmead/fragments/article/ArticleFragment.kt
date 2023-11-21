3348d2a6fc83ed298ad36a4506cf9c54b19b55f0
erik-lance <eriklance@gmail.com>
2023-11-22 02:04:01 +0800

    Refactored functions

--- app/src/main/java/com/newsmead/fragments/article/ArticleFragment.kt
+++ app/src/main/java/com/newsmead/fragments/article/ArticleFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
@@ -17,6 +16,7 @@ import com.newsmead.R
 import com.newsmead.databinding.FragmentArticleBinding
 import com.newsmead.databinding.ItemFeedArticleSimplifiedBinding
 import com.newsmead.fragments.layouts.BottomSheetDialogSaveFragment
+import com.newsmead.models.Article
 
 class ArticleFragment : Fragment() {
     private lateinit var binding: FragmentArticleBinding
@@ -27,35 +27,8 @@ class ArticleFragment : Fragment() {
     ): View? {
         binding = FragmentArticleBinding.inflate(inflater, container, false)
 
-        // Supply linear layout with FeedArticleSimplified items (not a recycler view)
-        val recommendedData = loadRecommendedArticlesData()
-        for (article in recommendedData) {
-            val itemFeedArticleSimplified = ItemFeedArticleSimplifiedBinding.inflate(inflater, container, false)
-            itemFeedArticleSimplified.tvArticleTitle.text = article.title
-            itemFeedArticleSimplified.tvSource.text = article.source
-            itemFeedArticleSimplified.tvArticleDate.text = article.date
-            itemFeedArticleSimplified.tvReadTime.text = article.readTime
-            itemFeedArticleSimplified.ivSourceImage.setImageResource(article.imageId)
-
-            // Add onClick to open article
-            itemFeedArticleSimplified.root.setOnClickListener {
-                val intent = Intent(requireContext(), ArticleActivity::class.java)
-                intent.putExtra("articleId", "TEST")
-                requireContext().startActivity(intent)
-            }
-
-            binding.llArticleRecommended.addView(itemFeedArticleSimplified.root)
-
-            // Add divider between items
-            val viewDivider = View(requireContext())
-            viewDivider.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1)
-            viewDivider.setBackgroundResource(R.drawable.line_divider)
-            binding.llArticleRecommended.addView(viewDivider)
-
-        }
-
-        // Set imageview
-        binding.ivSourceImage.setImageResource(R.drawable.sample_source_image)
+        // Provides Sample Article
+        loadMockArticleData(inflater, container)
 
 
         // Bottom sheet dialog for saving articles
@@ -89,7 +62,7 @@ class ArticleFragment : Fragment() {
                 action = Intent.ACTION_SEND
                 putExtra(Intent.EXTRA_TITLE, "NewsMead")
 
-                var articleText = binding.tvArticleHeadline.text.toString() + " by " + binding.tvSource.text.toString()
+                val articleText = binding.tvArticleHeadline.text.toString() + " by " + binding.tvSource.text.toString()
                 putExtra(Intent.EXTRA_TEXT, articleText)
                 type = "text/plain"
             }
@@ -98,6 +71,97 @@ class ArticleFragment : Fragment() {
             startActivity(shareIntent)
         }
 
+        addBottomAppBarListeners()
+
+        return binding.root
+    }
+
+    /**
+     * Loads mock article data for presentation purposes
+     * @param inflater LayoutInflater
+     * @param container ViewGroup?
+     */
+    private fun loadMockArticleData(inflater: LayoutInflater, container: ViewGroup?) {
+        // Supply linear layout with FeedArticleSimplified items (not a recycler view)
+        loadMockRecommendedArticles(inflater, container)
+
+        // Set imageview
+        binding.ivSourceImage.setImageResource(R.drawable.sample_source_image)
+    }
+
+    /**
+     * Loads mock recommended articles data for presentation purposes
+     * @param inflater LayoutInflater
+     * @param container ViewGroup?
+     */
+    private fun loadMockRecommendedArticles(inflater: LayoutInflater, container: ViewGroup?) {
+        val recommendedData = loadRecommendedArticlesData()
+        fillRecommendedArticlesRecyclerView(recommendedData)
+    }
+
+    /**
+     * Fills the recommended articles recycler view with data
+     * @param recommendedData ArrayList<Article>
+     */
+    private fun fillRecommendedArticlesRecyclerView(recommendedData: ArrayList<Article>) {
+        for (article in recommendedData) {
+            // Inflate item_feed_article_simplified.xml
+            val itemFeedArticleSimplified =
+                ItemFeedArticleSimplifiedBinding.inflate(layoutInflater, null, false)
+            itemFeedArticleSimplified.tvArticleTitle.text = article.title
+            itemFeedArticleSimplified.tvSource.text = article.source
+            itemFeedArticleSimplified.tvArticleDate.text = article.date
+            itemFeedArticleSimplified.tvReadTime.text = article.readTime
+            itemFeedArticleSimplified.ivSourceImage.setImageResource(article.imageId)
+
+            // Add onClick to open article
+            itemFeedArticleSimplified.root.setOnClickListener {
+                val intent = Intent(requireContext(), ArticleActivity::class.java)
+                intent.putExtra("articleId", "TEST")
+                requireContext().startActivity(intent)
+            }
+
+            binding.llArticleRecommended.addView(itemFeedArticleSimplified.root)
+
+            // Add divider between items
+            val viewDivider = View(requireContext())
+            viewDivider.layoutParams =
+                ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1)
+            viewDivider.setBackgroundResource(R.drawable.line_divider)
+            binding.llArticleRecommended.addView(viewDivider)
+        }
+    }
+
+    fun convertPixelsToSp(px: Float): Float {
+        return px / (requireContext().resources.displayMetrics.scaledDensity)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Add bottom app bar logic (it must stick and then only disappear
+        // once it reaches recommendations
+//        binding.btnArticleRecommendations.post {
+//            val btnLocation = IntArray(2)
+//            binding.btnArticleRecommendations.getLocationOnScreen(btnLocation)
+//            val btnY = btnLocation[1]
+//
+//            val nestedScrollView = binding.nsvArticleText
+//            val bottomAppBar = binding.bottomAppBar
+//            nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+//                Log.d("Y_COORDINATE", "btnY: $btnY scrollY: $scrollY")
+//                // Check if the scroll position is past the Y-coordinate of btnArticleRecommendations
+//                if (scrollY > btnY -1000) {
+//                    Log.d("SCROLL", "Hiding Bottom App Bar")
+//                    bottomAppBar.performHide()
+//                } else {
+//                    bottomAppBar.performShow()
+//                }
+//            })
+//        }
+
+
+    }
+
+    private fun addBottomAppBarListeners() {
         // Increase font size when btnArticleTextLarger is clicked
         binding.btnArticleTextLarger.setOnClickListener {
             binding.tvArticleText.textSize = convertPixelsToSp(binding.tvArticleText.textSize + 1)
@@ -135,35 +199,5 @@ class ArticleFragment : Fragment() {
             binding.btnArticleTextSmaller.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.sepia_btn))
             binding.btnSaveList.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.sepia_btn))
         }
-
-        return binding.root
-    }
-    fun convertPixelsToSp(px: Float): Float {
-        return px / (requireContext().resources.displayMetrics.scaledDensity)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // Add bottom app bar logic (it must stick and then only disappear
-        // once it reaches recommendations
-//        binding.btnArticleRecommendations.post {
-//            val btnLocation = IntArray(2)
-//            binding.btnArticleRecommendations.getLocationOnScreen(btnLocation)
-//            val btnY = btnLocation[1]
-//
-//            val nestedScrollView = binding.nsvArticleText
-//            val bottomAppBar = binding.bottomAppBar
-//            nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
-//                Log.d("Y_COORDINATE", "btnY: $btnY scrollY: $scrollY")
-//                // Check if the scroll position is past the Y-coordinate of btnArticleRecommendations
-//                if (scrollY > btnY -1000) {
-//                    Log.d("SCROLL", "Hiding Bottom App Bar")
-//                    bottomAppBar.performHide()
-//                } else {
-//                    bottomAppBar.performShow()
-//                }
-//            })
-//        }
-
-
     }
 }
