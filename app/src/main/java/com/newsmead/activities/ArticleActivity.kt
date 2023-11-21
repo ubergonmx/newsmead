8911ff7526e9bfb65a21a0dc058da97f90c1c3d5
erik-lance <eriklance@gmail.com>
2023-11-22 03:30:45 +0800

    Prepared safeargs navigation

--- app/src/main/java/com/newsmead/activities/ArticleActivity.kt
+++ app/src/main/java/com/newsmead/activities/ArticleActivity.kt
@@ -2,11 +2,15 @@ package com.newsmead.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navArgs
+import com.newsmead.R
 
 import com.newsmead.databinding.ActivityArticleBinding
-import com.newsmead.fragments.article.ArticleFragment
 
 class ArticleActivity : AppCompatActivity() {
+//    val args: ArticleActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,18 +18,35 @@ class ArticleActivity : AppCompatActivity() {
         val binding = ActivityArticleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        // Receive articleId from MainActivity
         val articleId = intent.getStringExtra("articleId")
 
-        if (articleId != null) {
-            val articleFragment = ArticleFragment()
-            val args = Bundle()
-            args.putString("articleId", articleId)
-            articleFragment.arguments = args
+        // Receive articleId from navigation
+//        val articleId = args.articleId
 
-            // Replace the fragment_container view with the ArticleFragment
-            supportFragmentManager.beginTransaction()
-                .replace(binding.flMainArticleContainer.id, articleFragment)
-                .commit()
-        }
+        // Update fragment with articleId
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.flMainArticleContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Retrieve article data
+        val articleTitle = "TEST"
+        val articleSource = "Article Source"
+        val articleDate = "Article Date" // No article date on article?
+        val articleReadTime = 0
+        val articleImage = "https://live.staticflickr.com/7221/7251835204_89c12bd6ef_b.jpg"
+        val articleBody = "Article Content"
+
+        // Creates a "safeargs" bundle
+        val bundle = bundleOf(
+            "articleId" to articleId,
+            "articleTitle" to articleTitle,
+            "articleSource" to articleSource,
+            "articleImage" to articleImage,
+            "articleBody" to articleBody,
+            "articleReadTime" to articleReadTime
+        )
+
+        navController.navigate(R.id.articleFragment, bundle)
     }
 }
