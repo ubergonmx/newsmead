44cfb73437cbc3c5d4573f83a9ab5f1a08c267d4
erik-lance <eriklance@gmail.com>
2023-11-22 08:15:01 +0800

    Added simpilified version of article adapter

---
+++ app/src/main/java/com/newsmead/recyclerviews/feed/ArticleSimplifiedAdapter.kt
@@ -0,0 +1,49 @@
+package com.newsmead.recyclerviews.feed
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.newsmead.databinding.ItemFeedArticleBinding
+import com.newsmead.models.Article
+
+class ArticleSimplifiedAdapter(
+    private val articleList: ArrayList<Article>,
+    private val clickListener: clickListener
+): RecyclerView.Adapter<ArticleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val itemViewBinding: ItemFeedArticleBinding = ItemFeedArticleBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ArticleViewHolder(itemViewBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        holder.bindData(articleList[position])
+
+        holder.itemView.setOnClickListener {
+            val parsedReadTime = articleList[position].readTime.split(" ")[0].toInt()
+            clickListener.onItemClicked(
+                articleId = articleList[position].newsId,
+                articleTitle = articleList[position].title,
+                articleSource = articleList[position].source,
+                articleImage = articleList[position].imageId.toString(),
+                articleReadTime = parsedReadTime
+            )
+        }
+    }
+
+    fun updateData(articles: List<Article>) {
+        articleList.clear()
+        articleList.addAll(articles)
+        notifyDataSetChanged()
+    }
+
+}
---
+++ app/src/main/java/com/newsmead/recyclerviews/feed/ArticleSimplifiedVewHolder.kt
@@ -0,0 +1,28 @@
+package com.newsmead.recyclerviews.feed
+
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.newsmead.databinding.ItemFeedArticleSimplifiedBinding
+import com.newsmead.fragments.article.ArticleSourceFragmentDirections
+import com.newsmead.models.Article
+
+class ArticleSimplifiedViewHolder(private val viewBinding: ItemFeedArticleSimplifiedBinding): RecyclerView.ViewHolder(viewBinding.root) {
+    // Performs the binding of the article data to the views in the ViewHolder
+    fun bindData(article: Article) {
+        this.viewBinding.tvSource.text = article.source
+        this.viewBinding.ivSourceImage.setImageResource(article.sourceImage)
+        this.viewBinding.tvArticleTitle.text = article.title
+        this.viewBinding.tvArticleDate.text = article.date
+        this.viewBinding.tvReadTime.text = article.readTime
+
+        this.viewBinding.root.setOnClickListener {v-> Navigation.findNavController(v)
+            .navigate(ArticleSourceFragmentDirections.actionArticleSourceFragmentToArticleFragment(
+                articleId = article.newsId,
+                articleTitle = article.title,
+                articleSource = article.source,
+                articleImage = article.imageId.toString(),
+                articleReadTime = article.readTime.toInt()
+            ))
+        }
+    }
+}
