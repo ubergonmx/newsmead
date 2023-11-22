f0c073bfaec85086f186aa00769c9e75a24f89ca
erik-lance <eriklance@gmail.com>
2023-11-22 09:30:19 +0800

    Added create list functionality button under saved

--- app/src/main/java/com/newsmead/fragments/saved/SavedListsFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedListsFragment.kt
@@ -8,8 +8,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.newsmead.data.DataHelper.loadListData
+import com.newsmead.data.FirebaseHelper
 import com.newsmead.databinding.FragmentSavedYourListsBinding
+import com.newsmead.fragments.layouts.NewListDialog
 import com.newsmead.models.SavedList
+import com.newsmead.recyclerviews.dialog.SheetDialogAdapter
 import com.newsmead.recyclerviews.saved.SavedListsAdapter
 
 class SavedListsFragment: Fragment() {
@@ -67,7 +70,18 @@ class SavedListsFragment: Fragment() {
 
         // Button for Create New List
         binding.btnCreateList.setOnClickListener {
+            val newListDialog = NewListDialog(requireContext()) { newListName ->
+                // Add new list to database
+                val result = FirebaseHelper.addListToFireStore(requireContext(), newListName)
 
+                if (result != "") {
+                    // Add new list to recyclerView
+                    val adapter = binding.rvSavedLists.adapter as SheetDialogAdapter
+                    val newList = SavedList(result, newListName)
+                    adapter.addNewList(newList)
+                }
+            }
+            newListDialog.show()
         }
 
         // Add spacing between items
