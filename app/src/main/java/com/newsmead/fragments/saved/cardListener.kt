a0ec4a15ea9fbc9377ee606150f1509b26b7be42
erik-lance <eriklance@gmail.com>
2023-11-23 16:15:15 +0800

    Added new cardListener interface for adapter

---
+++ app/src/main/java/com/newsmead/fragments/saved/cardListener.kt
@@ -0,0 +1,8 @@
+package com.newsmead.fragments.saved
+
+/**
+ * Interface for listening to card clicks in the SavedListsFragment.
+ */
+interface cardListener {
+    fun onCardClick(listId: String)
+}
