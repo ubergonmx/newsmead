f26e8692f364f4478df814d1880b12abfff8e3c9
erik-lance <eriklance@gmail.com>
2023-11-23 16:17:01 +0800

    Added proper list data passing and timing

--- app/src/main/java/com/newsmead/fragments/saved/SavedListsFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedListsFragment.kt
@@ -1,40 +1,45 @@
 package com.newsmead.fragments.saved
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.newsmead.data.DataHelper.loadListData
 import com.newsmead.data.FirebaseHelper
 import com.newsmead.databinding.FragmentSavedYourListsBinding
+import com.newsmead.fragments.bottomnavigation.SavedFragmentDirections
 import com.newsmead.fragments.layouts.NewListDialog
 import com.newsmead.models.SavedList
 import com.newsmead.recyclerviews.dialog.SheetDialogAdapter
 import com.newsmead.recyclerviews.saved.SavedListsAdapter
 
-class SavedListsFragment: Fragment() {
+class SavedListsFragment: Fragment(), cardListener {
     private lateinit var binding: FragmentSavedYourListsBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
+    private lateinit var data: ArrayList<SavedList>
+    private lateinit var adapter: SavedListsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentSavedYourListsBinding.inflate(layoutInflater)
         auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
-    }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        // Initialize recyclerView immediately
+        data = ArrayList<SavedList>()
+        adapter = SavedListsAdapter(ArrayList<SavedList>(), this)
+        binding.rvSavedLists.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.rvSavedLists.layoutManager = layoutManager
 
         // Get list of lists from firestore and move read later to top
-        val data = ArrayList<SavedList>()
-
         if (auth.currentUser != null) {
             val userId = auth.currentUser?.uid.toString()
             firestore.collection("users").document(userId).collection("lists").get()
@@ -54,22 +59,30 @@ class SavedListsFragment: Fragment() {
                         data.add(0, readLater)
                     }
 
-                    binding.rvSavedLists.adapter = SavedListsAdapter(data)
-                    val layoutManager = LinearLayoutManager(activity)
-                    layoutManager.orientation = LinearLayoutManager.VERTICAL
-
-                    binding.rvSavedLists.layoutManager = layoutManager
+                    Log.d("SavedListsFragment", "Retrieved data (${data.size}). Updating adapter")
+                    adapter.updateData(data)
                 }
                 .addOnFailureListener { exception ->
                     data += loadListData()
 
-                    binding.rvSavedLists.adapter = SavedListsAdapter(data)
-                    val layoutManager = LinearLayoutManager(activity)
-                    layoutManager.orientation = LinearLayoutManager.VERTICAL
-
-                    binding.rvSavedLists.layoutManager = layoutManager
+                    adapter.updateData(data)
                 }
+        } else {
+            data += loadListData()
+
+            adapter.updateData(data)
         }
+
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
 
         // Button for Create New List
         binding.btnCreateList.setOnClickListener {
@@ -91,5 +104,13 @@ class SavedListsFragment: Fragment() {
 //        val spacing = 48
 //        binding.rvSavedLists.addItemDecoration(com.newsmead.custom.CustomDividerSpacer(spacing))
 
+    }
+
+    override fun onCardClick(listId: String) {
+        Log.d("SavedListsFragment", "onCardClick: $listId")
+        // Action
+        val action = SavedFragmentDirections.actionSavedFragmentToSavedListArticlesFragment(listId)
+
+        Navigation.findNavController(requireView()).navigate(action)
     }
 }
--- app/src/main/java/com/newsmead/recyclerviews/saved/SavedListsAdapter.kt
+++ app/src/main/java/com/newsmead/recyclerviews/saved/SavedListsAdapter.kt
@@ -1,12 +1,18 @@
 package com.newsmead.recyclerviews.saved
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.newsmead.databinding.ItemSavedListBinding
+import com.newsmead.fragments.layouts.listListener
+import com.newsmead.fragments.saved.cardListener
 import com.newsmead.models.SavedList
 
-class SavedListsAdapter(private val listSavedList: ArrayList<SavedList>) : RecyclerView.Adapter<SavedListsViewHolder>(){
+class SavedListsAdapter(
+    private val listSavedList: ArrayList<SavedList>,
+    private val cardListener: cardListener
+) : RecyclerView.Adapter<SavedListsViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedListsViewHolder {
         val itemViewBinding: ItemSavedListBinding = ItemSavedListBinding.inflate(
@@ -29,6 +35,16 @@ class SavedListsAdapter(private val listSavedList: ArrayList<SavedList>) : Recyc
 
     override fun onBindViewHolder(holder: SavedListsViewHolder, position: Int) {
         holder.bindData(listSavedList[position])
+
+        // Add on click listener to card inside constraint layout
+        holder.getCardView().setOnClickListener {
+            cardListener.onCardClick(listSavedList[position].id)
+        }
+    }
+
+    fun updateData(savedLists: List<SavedList>) {
+        listSavedList.addAll(savedLists)
+        notifyDataSetChanged()
     }
 
 }
