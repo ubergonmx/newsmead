c674f0931b28789108098916e8939ca9a7711d42
erik-lance <eriklance@gmail.com>
2023-11-24 02:04:10 +0800

    Applied new adapter for checkbox

--- app/src/main/java/com/newsmead/fragments/saved/SavedListEditFragment.kt
+++ app/src/main/java/com/newsmead/fragments/saved/SavedListEditFragment.kt
@@ -6,22 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.newsmead.data.DataHelper
-import com.newsmead.data.FirebaseHelper
 import com.newsmead.databinding.FragmentSavedListEditBinding
+import com.newsmead.fragments.layouts.listListener
 import com.newsmead.models.Article
-import com.newsmead.recyclerviews.feed.ArticleAdapter
-import com.newsmead.recyclerviews.feed.clickListener
-import kotlinx.coroutines.launch
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
+import com.newsmead.recyclerviews.feed.ArticleEditAdapter
 
-class SavedListEditFragment: Fragment(), clickListener {
+class SavedListEditFragment: Fragment(), listListener {
     private val args: SavedListEditFragmentArgs by navArgs()
     private lateinit var binding: FragmentSavedListEditBinding
     private val articleIds = ArrayList<String>()
@@ -41,7 +34,7 @@ class SavedListEditFragment: Fragment(), clickListener {
         // Update name of list
         binding.tvSavedListEditName.text = args.listName
 
-        val adapter = ArticleAdapter(data, this)
+        val adapter = ArticleEditAdapter(data, this)
         binding.rvSavedEditArticles.adapter = adapter
 
         val layoutManager = LinearLayoutManager(context)
@@ -63,19 +56,22 @@ class SavedListEditFragment: Fragment(), clickListener {
         }
 
         // Save button
+        binding.btnEditSave.setOnClickListener {
+            // Save the checkedIds to the list
+            Log.d("SavedListEditArticles", "CheckedIds: $checkedIds")
+        }
 
 
         return binding.root
     }
 
-
-    override fun onItemClicked(
-        articleId: String,
-        articleTitle: String,
-        articleSource: String,
-        articleImage: String,
-        articleReadTime: Int
-    ) {
-        // Checkbox
+    override fun onListChecked(listId: String) {
+        // If checked, add to checkedIds
+        // If unchecked, remove from checkedIds
+        if (checkedIds.contains(listId)) {
+            checkedIds.remove(listId)
+        } else {
+            checkedIds.add(listId)
+        }
     }
 }
