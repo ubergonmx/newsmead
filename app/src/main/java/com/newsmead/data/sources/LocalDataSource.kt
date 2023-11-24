7a03938608f4c6569b12d1eabe7f78e9163e55fc
erik-lance <eriklance@gmail.com>
2023-11-24 18:44:45 +0800

    Now a RoomDatabase

--- app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
+++ app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
@@ -1,4 +1,11 @@
 package com.newsmead.data.sources
 
-class LocalDataSource {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.newsmead.models.NewsArticle
+import com.newsmead.models.NewsArticleDao
+
+@Database(entities = [NewsArticle::class], version = 1, exportSchema = false)
+abstract class LocalDataSource: RoomDatabase() {
+    abstract fun newsArticleDao(): NewsArticleDao
 }
