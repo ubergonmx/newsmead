976718603335bad34032887eeef4ca626c126bbd
erik-lance <eriklance@gmail.com>
2023-11-24 13:03:34 +0800

    Now displays history

--- app/src/main/java/com/newsmead/fragments/pages/HistoryFragment.kt
+++ app/src/main/java/com/newsmead/fragments/pages/HistoryFragment.kt
@@ -5,18 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.newsmead.data.DataHelper
 import com.newsmead.R
 import com.newsmead.custom.CustomDividerItemDecoration
+import com.newsmead.data.FirebaseHelper
 import com.newsmead.databinding.FragmentHistoryBinding
 import com.newsmead.models.Article
 import com.newsmead.recyclerviews.feed.ArticleSimplifiedAdapter
 import com.newsmead.recyclerviews.feed.clickListener
+import kotlinx.coroutines.launch
 
 class HistoryFragment: Fragment(), clickListener {
     private lateinit var binding: FragmentHistoryBinding
+    private lateinit var adapter: ArticleSimplifiedAdapter
+    private lateinit var data: ArrayList<Article>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +29,7 @@ class HistoryFragment: Fragment(), clickListener {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        data = ArrayList()
 
         // Back Button
         binding.btnHistoryBack.setOnClickListener {
@@ -32,22 +38,28 @@ class HistoryFragment: Fragment(), clickListener {
 
         // RecyclerView
         // val data = DataHelper.loadArticleData()
-        binding.rvHistory.adapter = ArticleSimplifiedAdapter(ArrayList(), this)
-        // Load data and update adapter
-        DataHelper.loadArticleData { articles ->
-            // Stop the shimmer
-            binding.shimmerHistory.stopShimmer()
-            binding.shimmerHistory.visibility = View.GONE
-
-            // Update the adapter with the data
-            (binding.rvHistory.adapter as ArticleSimplifiedAdapter).updateData(articles)
-        }
+        adapter = ArticleSimplifiedAdapter(data, this)
+        binding.rvHistory.adapter = adapter
         val layoutManager = LinearLayoutManager(requireContext())
         binding.rvHistory.layoutManager = layoutManager
 
         // Divider
         val customDivider = CustomDividerItemDecoration(context, R.drawable.line_divider)
         binding.rvHistory.addItemDecoration(customDivider)
+
+        // Load history data
+        lifecycleScope.launch {
+            val historyData = FirebaseHelper.getHistory(requireContext())
+
+            // Stop loading animation
+            binding.shimmerHistory.stopShimmer()
+            binding.shimmerHistory.visibility = View.GONE
+
+            if (historyData.isNotEmpty()) {
+                data.addAll(historyData)
+                adapter.notifyDataSetChanged()
+            }
+        }
 
 
         return binding.root
