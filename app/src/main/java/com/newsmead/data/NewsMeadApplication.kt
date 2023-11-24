ca008d5df9e70d2cb012a31dd62e2d705c5e0d04
erik-lance <eriklance@gmail.com>
2023-11-24 18:46:23 +0800

    Added comment to clarify what app is

--- app/src/main/java/com/newsmead/data/NewsMeadApplication.kt
+++ app/src/main/java/com/newsmead/data/NewsMeadApplication.kt
@@ -2,6 +2,10 @@ package com.newsmead.data
 
 import android.app.Application
 
+/**
+ * Application class to initialize the Room database. This will be called before any other
+ * component is initialized.
+ */
 class NewsMeadApplication: Application() {
     override fun onCreate() {
         super.onCreate()
