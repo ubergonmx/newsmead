0c1b1a8656a3f1f745d51f37bdd32b2a6a3404a2
erik-lance <eriklance@gmail.com>
2023-11-24 18:53:26 +0800

    Added companion object for receiving instance

--- app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
+++ app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
@@ -1,6 +1,8 @@
 package com.newsmead.data.sources
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.newsmead.models.NewsArticle
 import com.newsmead.models.NewsArticleDao
@@ -8,4 +10,21 @@ import com.newsmead.models.NewsArticleDao
 @Database(entities = [NewsArticle::class], version = 1, exportSchema = false)
 abstract class LocalDataSource: RoomDatabase() {
     abstract fun newsArticleDao(): NewsArticleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LocalDataSource? = null
+
+        fun getInstance(context: Context): LocalDataSource {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    LocalDataSource::class.java,
+                    "news_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
