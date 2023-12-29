ee8fea93b50726203a8f37a4d576a14faf1729e4
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-12-29 19:23:36 +0800

    Now updates persistent data on startup

--- app/src/main/java/com/newsmead/data/NewsMeadApplication.kt
+++ app/src/main/java/com/newsmead/data/NewsMeadApplication.kt
@@ -1,14 +1,27 @@
 package com.newsmead.data
 
 import android.app.Application
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 /**
  * Application class to initialize the Room database. This will be called before any other
  * component is initialized.
  */
 class NewsMeadApplication: Application() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate() {
         super.onCreate()
         DatabaseHelper.initDatabase(this)
+
+        // Check if logged in
+        // If logged in, then preload data
+        if (FirebaseHelper.getUid() != "null") {
+            GlobalScope.launch {
+                val pairData = FirebaseHelper.getListsAndArticles(this@NewsMeadApplication)
+                PreloadedData.updateSavedData(pairData)
+            }
+        }
     }
 }
