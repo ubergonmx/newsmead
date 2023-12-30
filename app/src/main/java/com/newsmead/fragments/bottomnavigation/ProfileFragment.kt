2523fea65ca08b3b98040cd6134af7cc22f33cd2
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-12-30 13:35:18 +0800

    Refactored and now clears data upon logout

--- app/src/main/java/com/newsmead/fragments/bottomnavigation/ProfileFragment.kt
+++ app/src/main/java/com/newsmead/fragments/bottomnavigation/ProfileFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.auth.FirebaseAuth
 import com.newsmead.activities.AccountActivity
+import com.newsmead.data.PreloadedData
 
 // ViewBinding
 import com.newsmead.databinding.FragmentProfileBinding
@@ -25,19 +26,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var viewBinding: FragmentProfileBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
         // Initialize viewBinding for ProfileFragment
         this.viewBinding = FragmentProfileBinding.inflate(layoutInflater)
@@ -51,6 +45,7 @@ class ProfileFragment : Fragment() {
         // Buttons
         this.viewBinding.btnLogout.setOnClickListener {
             this.auth.signOut()
+            PreloadedData.clearData()
 
             // Finish Activity and Go to AccountActivity and reset the back stack
             val intent = Intent(requireActivity(), AccountActivity::class.java)
@@ -92,26 +87,6 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return this.viewBinding.root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     private fun updateProfile(name: String, password: String, confirmPassword: String): Boolean {
