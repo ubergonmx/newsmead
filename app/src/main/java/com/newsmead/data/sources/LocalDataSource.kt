1071255d8718b92f188ea37bd7edd4772a1f4449
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-01-29 08:16:37 +0800

    Added migration script for 1 to 2

--- app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
+++ app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.newsmead.models.NewsArticle
 import com.newsmead.models.NewsArticleDao
 
@@ -15,13 +17,25 @@ abstract class LocalDataSource: RoomDatabase() {
         @Volatile
         private var INSTANCE: LocalDataSource? = null
 
+        /**
+         * Migration from version 1 to 2.
+         * Added article_body and last_updated column to news_article table.
+         */
+        private val migration1to2 = object: Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE news_article ADD COLUMN article_body TEXT NOT NULL DEFAULT ")
+                db.execSQL("ALTER TABLE news_article ADD COLUMN last_updated INTEGER NOT NULL DEFAULT 0")
+            }
+        }
+
         fun getInstance(context: Context): LocalDataSource {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     LocalDataSource::class.java,
                     "news_database"
-                ).build()
+                ).addMigrations(migration1to2)
+                    .build()
                 INSTANCE = instance
                 instance
             }
