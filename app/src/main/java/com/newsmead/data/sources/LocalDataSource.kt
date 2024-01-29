f770bb9ae00f45b17161b0cae6a76d43f02cf2da
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-01-29 08:07:28 +0800

    Updated Room Version

--- app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
+++ app/src/main/java/com/newsmead/data/sources/LocalDataSource.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.newsmead.models.NewsArticle
 import com.newsmead.models.NewsArticleDao
 
-@Database(entities = [NewsArticle::class], version = 1, exportSchema = false)
+@Database(entities = [NewsArticle::class], version = 2, exportSchema = false)
 abstract class LocalDataSource: RoomDatabase() {
     abstract fun newsArticleDao(): NewsArticleDao
 
