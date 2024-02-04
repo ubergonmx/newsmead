ea6e196642a88a0ab2f4c8498cb5dbc7f9217532
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-05 07:10:36 +0800

    Refactor DataHelper to use only one method

--- app/src/main/java/com/newsmead/data/DataHelper.kt
+++ app/src/main/java/com/newsmead/data/DataHelper.kt
@@ -7,14 +7,10 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.newsmead.R
-import com.newsmead.data.sources.RemoteDataSource
 import com.newsmead.models.Article
-import com.newsmead.models.NewsResponse
 import java.text.DateFormat
 import java.util.Date
 
-import com.newsmead.models.SavedList
-import com.newsmead.network.NewsAPIClientFactory
 import java.util.Locale
 import kotlin.collections.ArrayList
 
@@ -60,19 +56,51 @@ object DataHelper {
         }
     }
 
+    // Reverse source name mapping
+    fun reverseSourceNameMap(fullName: String): String {
+        return when (fullName) {
+            "GMA News" -> "gmanews"
+            "INQUIRER.NET" -> "inquirer"
+            "Philstar" -> "philstar"
+            "The Manila Bulletin" -> "manilabulletin"
+            "TV5 News" -> "news5"
+            else -> ""
+        }
+    }
 
-    fun loadArticleData(context: Context?, source: String? = null, callback: (List<Article>) -> Unit) {
+
+    fun loadArticleData(
+        context: Context?,
+        page: Int? = 1,
+        source: String? = null,
+        category: String? = null,
+        searchText : String? = null,
+        callback: (List<Article>) -> Unit)
+    {
         // Create an empty ArrayList
         val articles = ArrayList<Article>()
 
-        // Create random value between 1-100
-        val random = (1..100).random()
+        val baseUrl = "https://newsmead.southeastasia.cloudapp.azure.com"
+        var articleUrl = "$baseUrl/articles/?page=$page"
+        
+        val uid = FirebaseHelper.getUid()
+        var url = "$baseUrl/recommendations/$uid/$page"
+
+        if (category != null) {
+            articleUrl += "&category=$category"
+        }
+        if (source != null) {
+            articleUrl += "&source=$source"
+        }
+        if (searchText != null) {
+            articleUrl += "&text=$searchText"
+        }
+
+        if (searchText != null || source != null || category != null) {
+            url = articleUrl
+        }
 
         // Fetch articles using Volley
-        var url = "https://newsmead.southeastasia.cloudapp.azure.com/articles/?page=$random"
-        if (source != null) {
-            url += "&source=$source"
-        }
         val queue = Volley.newRequestQueue(context)
 
         // Request a JsonObject response from the provided URL.
@@ -137,35 +165,6 @@ object DataHelper {
             })
 
         queue.add(jsonObjectRequest)
-
-        // Fetch top headlines using the repository
-//        dataRepository.getTopHeadlines("ph", "13328281630540aaa6c2750b76b5ee12", object : DataRepository.DataCallback<NewsResponse> {
-//            override fun onSuccess(response: NewsResponse) {
-//                // Process the data here
-//                for (data in response.articles) {
-//                    Log.d("DataHelper", data.toString())
-//                    data.author?.let {
-//                        Article(
-//                            it,
-//                            formatTitle(data.title),
-//                            formatDate(data.publishedAt),
-//                            (3..9).random().toString() + " min read",
-//                            data.url
-//                        )
-//                    }?.let {
-//                        articles.add(
-//                            it
-//                        )
-//                    }
-//                }
-//                callback(articles)
-//            }
-//
-//            override fun onError(error: Throwable) {
-//                // Handle errors
-//
-//            }
-//        })
     }
 
 
@@ -173,64 +172,6 @@ object DataHelper {
     // ----------------------------------------------------------------------------- //
     // ----------------------------- Data for Testing ------------------------------ //
     // ----------------------------------------------------------------------------- //
-    fun loadArticleData(): ArrayList<Article> {
-        val data = ArrayList<Article>()
-        data.add(
-            Article(
-                "CNN",
-                "Biden's first 100 days: What he's gotten done",
-                "Apr 29, 2021",
-                "9 min read",
-                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-            )
-        )
-        data.add(
-            Article(
-                "INQUIRER.NET",
-                "BTS to perform new single 'Butter' at Billboard Music Awards",
-                "Jan 28, 2022",
-                "3 min read",
-                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-            )
-        )
-        data.add(
-            Article(
-                "ABS-CBN News",
-                "Showbiz couple Kylie Padilla, Aljur Abrenica split",
-                "Mar 12, 2023",
-                "4 min read",
-                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-            )
-        )
-        data.add(
-            Article(
-                "Rappler",
-                "Spacex launches 60 more Starlink satellites into orbit",
-                "Dec 29, 2022",
-                "5 min read",
-                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-            )
-        )
-        data.add(
-            Article(
-                "Manila Bulletin",
-                "Duterte to meet with Chinese envoy over West Philippine Sea issue",
-                "Nov 17, 2020",
-                "7 min read",
-                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-            )
-        )
-        data.add(
-            Article(
-                "Philstar",
-                "Comelec: 59 party-list groups to join 2022 polls",
-                "Jul 02, 2022",
-                "4 min read",
-                "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-            )
-        )
-        return data
-    }
 
     fun loadArticleDataLatest(): ArrayList<Article> {
         val data = ArrayList<Article>()
@@ -262,139 +203,6 @@ object DataHelper {
             )
         )
         return data
-    }
-
-    fun loadRecommendedArticlesData(): ArrayList<Article> {
-        val data = ArrayList<Article>()
-        data.add(
-            Article(
-                "CNN Philippines",
-                "Recommended Article 1",
-                "Oct 20, 2023",
-                "9 min read",
-                "http://www.cnnphilippines.com/news/2023/10/20/marcos-says-15k-filipinos-to-benefit-with-saudi-deal.html"
-            )
-        )
-        data.add(
-            Article(
-                "INQUIRER.NET",
-                "Recommended Article 2",
-                "Oct 19, 2023",
-                "9 min read",
-                "http://www.cnnphilippines.com/news/2023/10/20/marcos-says-15k-filipinos-to-benefit-with-saudi-deal.html"
-            )
-        )
-        data.add(
-            Article(
-                "ABS-CBN News",
-                "Recommended Article 3",
-                "Oct 18, 2023",
-                "9 min read",
-                "http://www.cnnphilippines.com/news/2023/10/20/marcos-says-15k-filipinos-to-benefit-with-saudi-deal.html"
-            )
-        )
-        data.add(
-            Article(
-                "Rappler",
-                "Recommended Article 4",
-                "Oct 17, 2023",
-                "9 min read",
-                "http://www.cnnphilippines.com/news/2023/10/20/marcos-says-15k-filipinos-to-benefit-with-saudi-deal.html"
-            )
-        )
-        return data
-    }
-
-    fun loadSearchArticlesData(searchQuery : String, callback: (List<Article>) -> Unit) {
-        // Create an empty ArrayList
-        val articles = ArrayList<Article>()
-
-        // Create an instance of the DataRepository
-        val dataRepository = DataRepository(RemoteDataSource(NewsAPIClientFactory.create()))
-
-        // Fetch top headlines using the repository
-        dataRepository.getSearchedArticles(searchQuery, "13328281630540aaa6c2750b76b5ee12", object : DataRepository.DataCallback<NewsResponse> {
-            override fun onSuccess(response: NewsResponse) {
-                // Process the data here
-                for (data in response.articles) {
-                    Log.d("DataHelper", data.toString())
-                    data.author?.let {
-                        Article(
-                            it,
-                            data.title,
-                            formatDate(data.publishedAt),
-                            (3..9).random().toString() + " min read",
-                            data.url
-                        )
-                    }?.let {
-                        articles.add(
-                            it
-                        )
-                    }
-                }
-                callback(articles)
-            }
-
-            override fun onError(error: Throwable) {
-                // Handle errors
-                Log.e("DataHelper", error.toString())
-                articles.add(
-                    Article(
-                        "CNN",
-                        "Biden's first 100 days: What he's gotten done",
-                        "Apr 29, 2021",
-                        "9 min read",
-                        "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-                    )
-                )
-                articles.add(
-                    Article(
-                        "INQUIRER.NET",
-                        "BTS to perform new single 'Butter' at Billboard Music Awards",
-                        "Jan 28, 2022",
-                        "3 min read",
-                        "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-                    )
-                )
-                articles.add(
-                    Article(
-                        "ABS-CBN News",
-                        "Showbiz couple Kylie Padilla, Aljur Abrenica split",
-                        "Mar 12, 2023",
-                        "4 min read",
-                        "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-                    )
-                )
-                articles.add(
-                    Article(
-                        "Rappler",
-                        "Spacex launches 60 more Starlink satellites into orbit",
-                        "Dec 29, 2022",
-                        "5 min read",
-                        "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-                    )
-                )
-                articles.add(
-                    Article(
-                        "Manila Bulletin",
-                        "Duterte to meet with Chinese envoy over West Philippine Sea issue",
-                        "Nov 17, 2020",
-                        "7 min read",
-                        "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-                    )
-                )
-                articles.add(
-                    Article(
-                        "Philstar",
-                        "Comelec: 59 party-list groups to join 2022 polls",
-                        "Jul 02, 2022",
-                        "4 min read",
-                        "https://www.cnn.com/2021/04/29/politics/biden-first-100-days/index.html"
-                    )
-                )
-                callback(articles)
-            }
-        })
     }
 
     fun loadCategoryArticlesData(): ArrayList<Article> {
@@ -473,9 +281,6 @@ object DataHelper {
         data.add("Lifestyle")
         data.add("Business")
         data.add("Entertainment")
-        data.add("Health & Fitness")
-        data.add("Travel")
-        data.add("Money")
         return data
     }
 
@@ -486,34 +291,6 @@ object DataHelper {
         data.add("Philstar")
         data.add("The Manila Bulletin")
         data.add("TV5 News")
-        return data
-    }
-
-    fun loadListData(): ArrayList<SavedList> {
-        val data = ArrayList<SavedList>()
-        data.add(SavedList("A", "Read Later", loadArticleDataLatest()))
-        data.add(SavedList("B", "Politics"))
-        data.add(SavedList("C", "Sports"))
-        data.add(SavedList("D", "manila updates"))
-        data.add(SavedList("E", "Business"))
-        data.add(SavedList("F", "Health Advice"))
-        data.add(SavedList("G", "Opinion"))
-        data.add(SavedList("H", "important news"))
-        data.add(SavedList("I", "NBA"))
-        return data
-    }
-
-    fun loadListNamesData(): ArrayList<String> {
-        val data = ArrayList<String>()
-        data.add("Read Later")
-        data.add("Politics")
-        data.add("Sports")
-        data.add("manila updates")
-        data.add("Business")
-        data.add("Health Advice")
-        data.add("Opinion")
-        data.add("important news")
-        data.add("NBA")
         return data
     }
 
